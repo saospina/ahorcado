42ff790bb1953c46de5fb1aa38417c12837791b3
saospina <sergos0617@gmail.com>
2018-09-02 09:56:11 -0500

    Creating dynamic letters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+//Decorador, agrega funcionalidades a la clase de forma externa
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  letras = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+  palabra = 'yo';
+  palabraOculta = '';
+
+  constructor(){
+    this.palabraOculta='_ '.repeat(this.palabra.length);
+  }
 }
