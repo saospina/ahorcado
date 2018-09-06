ddc1c109643d87eb9eb743a4077c3cc66da34fdd
saospina <sergos0617@gmail.com>
2018-09-05 22:50:33 -0500

    Added function find the missing word

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  letras = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O',
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O',
     'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
-  palabra = 'yo';
-  palabraOculta = '';
+  palabra = 'AGUACATE';
+  palabraOculta = ' ';
 
-  constructor(){
-    this.palabraOculta='_ '.repeat(this.palabra.length);
+  constructor() {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+  comprobar(letra) {
+    console.log('letra', letra);
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra)  {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
   }
 }
