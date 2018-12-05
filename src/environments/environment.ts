46dfba820e81976613ce8012dfe7c026d6cca668
sergio-ospina-yuxi <sergio.ospina@yuxiglobal.com>
2018-12-05 13:06:14 -0500

    Github pages1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false
 };
 
+
+
 /*
  * In development mode, for easier debugging, you can ignore zone related error
  * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
